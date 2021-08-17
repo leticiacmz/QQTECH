25ac9c2cdc971a0686b0c2e9afb1ceccca2c7d3d
leticiacmz <leticmz@gmail.com>
2021-08-17 20:30:50 -0300

    feat: exercices about group by, sum, min,  max,avg

--- 12 - Insert, Update, Delete/Script - Lojas_ABC.sql
+++ 12 - Insert, Update, Delete/Script - Lojas_ABC.sql
@@ -169,3 +169,41 @@ select item_pedido_venda.id_produto, produto.valor_produto  from lojas_abc.item_
 where produto.valor_produto >= '3000'
 order by id_produto 
 
+
+select 
+max(data_nascimento) as mais_novo,
+min(data_nascimento) as mais_velho
+from lojas_abc.cliente 
+
+
+select
+max(valor_produto) as maior_valor,
+min(valor_produto) as menor_valor,
+sum(valor_produto) as total,
+avg(valor_produto) as média
+from lojas_abc.produto
+
+
+
+
+select id_pedido,
+	sum(valor_produto) as total
+ from lojas_abc.item_pedido_venda
+ group by id_pedido
+ order by id_pedido 
+ 
+ select id_cliente,
+sum(id_cliente) qtd_pedidos
+from lojas_abc.cliente
+group by id_cliente
+ order by id_cliente
+
+select id_pedido, cliente.id_cliente, 
+count(id_pedido) qtd_pedidos
+from lojas_abc.item_pedido_venda, lojas_abc.cliente 
+group by id_pedido, id_cliente
+having count(*) > 1
+order by count(id_pedido);
+
+
+
--- 13 - Select, In, Like, Order by, Distinct/Script-Lojas_Abc.sql
+++ 13 - Select, In, Like, Order by, Distinct/Script-Lojas_Abc.sql
@@ -9,6 +9,7 @@ select nome_completo, data_nascimento, origem from lojas_abc.cliente
 order by nome_completo, data_nascimento 
 
 select distinct id_cliente from lojas_abc.pedido
+order by id_cliente 
 
 select nome_produto, valor_produto from lojas_abc.produto
 where nome_produto like '%nokia%' or nome_produto like '%NOKIA%'
@@ -23,3 +24,14 @@ where data_nascimento::date > '1950-01-01' or data_nascimento::date < '2000-12-3
 select item_pedido_venda.id_produto, produto.valor_produto  from lojas_abc.item_pedido_venda, lojas_abc.produto
 where produto.valor_produto >= '3000'
 order by id_produto 
+
+select
+max(valor_produto) maior_valor,
+min(valor_produto) menor_valor,
+sum(valor_produto) total,
+avg(valor_produto) média_valor
+ from lojas_abc.produto
+
+ select id_pedido,
+ sum(valor_produto) total
+ from lojas_abc.item_pedido_venda ipv 
