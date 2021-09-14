93aafcdef28ccd74c51f75e5260ccb22aa6ec31f
Leticia Cruz Menezes <leticmz@gmail.com>
2021-09-13 21:21:07 -0300

    fetch: new Javascript classes

---
+++ 03 - Javascript/31-Intro/variaveis.js
@@ -0,0 +1,5 @@
+let clientes = [];
+let cliente1 = clientes.push({ nome: "Leticia", idade: 23 });
+let cliente3 = clientes.push("Roberta");
+let cliente2 = clientes.push({ nome: "Joaozinho", idade: 34 });
+console.log(clientes);
